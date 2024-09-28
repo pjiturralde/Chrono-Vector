e2e1bd90580e296d3cf601b33e4b2b36f5d84b4f
DripKnight <90874883+DripKnight@users.noreply.github.com>
2024-09-28 16:59:07 -0700

    Added tests for Projectile class

---
+++ src/test/model/ProjectileTest.java
@@ -0,0 +1,79 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ProjectileTest {
+    private Projectile p1;
+    private Projectile p2;
+
+    @BeforeEach
+    void runBefore() {
+        p1 = new Projectile(0, 0, 1, 1);
+        p2 = new Projectile(1, 2, 1, 0);
+    }
+
+    @Test
+    void additionTest() {
+        p1.changeDirection(3, 2);
+        int pointX = p1.getDirection().getX();
+        int pointY = p1.getDirection().getY();
+
+        assertEquals(pointX, 3);
+        assertEquals(pointY, 2);
+    }
+
+    @Test
+    void moveForwardTest() {
+        p1.moveForward();
+
+        int pointX = p1.getPosition().getX();
+        int pointY = p1.getPosition().getY();
+
+        assertEquals(pointX, 1);
+        assertEquals(pointY, 1);
+    }
+
+    @Test
+    void moveBackwardTest() {
+        p1.moveBackward();
+
+        int pointX = p1.getPosition().getX();
+        int pointY = p1.getPosition().getY();
+
+        assertEquals(pointX, -1);
+        assertEquals(pointY, -1);
+    }
+
+    @Test
+    void getPositionTest() {
+        int pointX = p1.getPosition().getX();
+        int pointY = p1.getPosition().getY();
+
+        assertEquals(pointX, 0);
+        assertEquals(pointY, 0);
+
+        pointX = p2.getPosition().getX();
+        pointY = p2.getPosition().getY();
+
+        assertEquals(pointX, 1);
+        assertEquals(pointY, 2);
+    }
+
+    @Test
+    void getDirectionTest() {
+        int pointX = p1.getDirection().getX();
+        int pointY = p1.getDirection().getY();
+
+        assertEquals(pointX, 1);
+        assertEquals(pointY, 1);
+
+        pointX = p2.getDirection().getX();
+        pointY = p2.getDirection().getY();
+
+        assertEquals(pointX, 1);
+        assertEquals(pointY, 0);
+    }
+}
