cf4ff5949b6bf5b692cc12d15dc8fa97ea0ffdd0
DripKnight <90874883+DripKnight@users.noreply.github.com>
2024-09-28 16:26:18 -0700

    Added specification of Projectile class

---
+++ src/main/model/Projectile.java
@@ -0,0 +1,36 @@
+package model;
+
+// A class representing a projectile with a position, direction, default speed, and speed
+public class Projectile {
+    private Vector2 cellPosition;
+    private Vector2 directionVector;
+
+    // EFFECTS: constructs a Projectile object
+    public Projectile(int posX, int posY, int dirX, int dirY, int speed) {
+        // stub
+    }
+
+    // REQUIRES: direction cannot be a zero vector
+    // MODIFIES: this
+    // EFFECTS: changes current direction of Projectile to the given direction
+    public void changeDirection(Vector2 direction) {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: moves projectile forwards depending on direction and speed
+    public void moveForward() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: moves projectile backwards depending on direction and speed
+    public void moveBackward() {
+        // stub
+    }
+
+    // EFFECTS: returns the Projectile's position
+    public Vector2 getPosition() {
+        return null; // stub
+    }
+}
