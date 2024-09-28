b6c9ce7c028c983e9cde88962fcd9f2137b47ef9
DripKnight <90874883+DripKnight@users.noreply.github.com>
2024-09-28 16:05:47 -0700

    Added implementation for Vector2 class

--- src/main/model/Vector2.java
+++ src/main/model/Vector2.java
@@ -7,38 +7,41 @@ public class Vector2 {
 
     // EFFECTS: constructs a Vector2 object
     public Vector2(int posX, int posY) {
-        // stub
+        this.posX = posX;
+        this.posY = posY;
     }
 
     // MODIFIES: this
     // EFFECTS: adds this Vector2 and other Vector2 together
     public void Add(Vector2 other) {
-        // stub
+        this.posX += other.posX;
+        this.posY += other.posY;
     }
 
     // MODIFIES: this
     // EFFECTS: subtracts other Vector2 from this Vector2
     public void Subtract(Vector2 other) {
-        // stub
+        this.posX -= other.posX;
+        this.posY -= other.posY;
     }
     
     // EFFECTS: returns if this Vector2 and other Vector2 have equal coordinates
     public boolean isEqual(Vector2 other) {
-        return false; // stub
+        return posX == other.posX && posY == other.posY;
     }
 
     // EFFECTS: returns a new Vector2 scaled by the given scalar value
     public Vector2 Scale(int scalar) {
-        return null; // stub
+        return new Vector2(this.posX * scalar, this.posY * scalar);
     }
 
     // EFFECTS: returns posX
     public int getX() {
-        return -1; // stub
+        return posX;
     }
 
     // EFFECTS: returns posY
     public int getY() {
-        return -1; // stub
+        return posY;
     }
 }
