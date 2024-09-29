87e955823f6cf31f7975939a55eeecf1e6883801
DripKnight <90874883+DripKnight@users.noreply.github.com>
2024-09-28 17:20:37 -0700

    Added specification of Wall class

---
+++ src/main/model/Wall.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Wall {
+    private Vector2 size;
+    private Vector2 position;
+
+    // EFFECTS: constructs a Wall object
+    public Wall(int posX, int posY, int sizeX, int sizeY) {
+        // stub
+    }
+
+    // EFFECTS: returns size vector
+    public Vector2 getSize() {
+        return null; // stub
+    }
+
+    // EFFECTS: returns position vector
+    public Vector2 getPosition() {
+        return null; // stub
+    }
+}
