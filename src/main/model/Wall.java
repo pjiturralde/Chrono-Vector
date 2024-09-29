040586d8f0aded5bd1292165c8926108a88ed8ae
DripKnight <90874883+DripKnight@users.noreply.github.com>
2024-09-28 17:37:32 -0700

    Added implementation for Wall class

--- src/main/model/Wall.java
+++ src/main/model/Wall.java
@@ -6,16 +6,17 @@ public class Wall {
 
     // EFFECTS: constructs a Wall object
     public Wall(int posX, int posY, int sizeX, int sizeY) {
-        // stub
+        this.position = new Vector2(posX, posY);
+        this.size = new Vector2(sizeX, sizeY);
     }
 
     // EFFECTS: returns size vector
     public Vector2 getSize() {
-        return null; // stub
+        return size; // stub
     }
 
     // EFFECTS: returns position vector
     public Vector2 getPosition() {
-        return null; // stub
+        return position; // stub
     }
 }
