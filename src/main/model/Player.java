63f8119957c8bd06fd57b2ffc3e3a1a34531a2a4
DripKnight <90874883+DripKnight@users.noreply.github.com>
2024-09-28 17:57:58 -0700

    Added specification of Player class

---
+++ src/main/model/Player.java
@@ -0,0 +1,23 @@
+package model;
+
+// A class representing a Player object
+public class Player {
+    Vector2 position;
+
+    // EFFECTS: constructs a Player object
+    public Player(int posX, int posY) {
+        // stub
+    }
+
+    // REQUIRES: (dirX, dirY) must be normalized eg. (1,0), (0,1), (-1, 0), or (0,-1)
+    // MODIFIES: this
+    // EFFECTS: moves player one cell in the given direction
+    public void move(int dirX, int dirY) {
+        // stub
+    }
+
+    // EFFECTS: returns Player's position
+    public Vector2 getPosition() {
+        return null; // stub
+    }
+}
