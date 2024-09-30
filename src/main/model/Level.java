84d05059365a95245f773cebf7a531ae0d65e8bd
DripKnight <90874883+DripKnight@users.noreply.github.com>
2024-09-30 15:36:01 -0700

    Added implementation for Level class

--- src/main/model/Level.java
+++ src/main/model/Level.java
@@ -1,11 +1,13 @@
 package model;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.ArrayList;
 
 // A class representing a Level object
 public class Level {
     private String name;
+    private boolean levelLost;
     private Vector2 timeDirection;
     private LinkedList<Projectile> projectileList;
     private ArrayList<Wall> wallList;
@@ -15,19 +17,23 @@ public class Level {
     //           eg. (0,1), (1,0), (-1, 0), ...
     // EFFECTS: constructs a Level object with an empty projectileList and empty wallList
     public Level(String name, int timeDirX, int timeDirY) {
-        // stub
+        this.name = name;
+        this.timeDirection = new Vector2(timeDirX, timeDirY);
+        this.projectileList = new LinkedList<Projectile>();
+        this.wallList = new ArrayList<Wall>();
+        this.levelLost = false;
     }
 
     // MODIFIES: this
     // EFFECT: adds Projectile object to list of Projectiles
     public void addProjectile(Projectile p) {
-        // stub
+        projectileList.addLast(p);
     }
 
     // MODIFIES: this
     // EFFECT: adds Wall object to list of Walls
     public void addWall(Wall w) {
-        // stub
+        wallList.addLast(w);
     }
 
     // REQUIRES: either moveDirX or moveDirY has to be 0 and
@@ -35,30 +41,88 @@ public class Level {
     //           eg. (0,1), (1,0), (-1, 0), ...
     // EFFECTS: checks if player moves in the same direction as timeDirection
     public void checkPlayerMovement(int moveDirX, int moveDirY) {
-        // stub
+        Vector2 playerMovementDir = new Vector2(moveDirX, moveDirY);
+        Vector2 oppositeTimeDir = Vector2.scale(timeDirection, -1);
+
+        if (playerMovementDir.isEqualTo(timeDirection)) {
+            moveAllProjectiles(1);
+        } else if (playerMovementDir.isEqualTo(oppositeTimeDir)) {
+            moveAllProjectiles(-1);
+        }
     }
 
-    // REQUIRES: timeScale must be either 1, 0, or -1
+    // REQUIRES: timeScale must be either 1, or -1
     // MODIFIES: this
     // EFFECTS: calls either moveForward or moveBackward on every projectile based on given timeScale
     private void moveAllProjectiles(int timeScale) {
-        // stub
+        Iterator<Projectile> iterator = projectileList.iterator();
+
+        while (iterator.hasNext()) {
+            Projectile projectile = iterator.next();
+
+            if (timeScale == 1) {
+                projectile.moveForward();
+            } else {
+                projectile.moveBackward();
+            }   
+        }
     }
 
+    // REQUIRES: moveDirX and moveDirY can't both be zero and as a vector has to take the form
+    //           (0, 1), (1, 0), (0, -1), or (-1, 0)
+    // MODIFIES: Player p
     // EFFECTS: checks if given player collides with anything in the Level
     //          if player collides with wall move him backo nce
     //          if player collides with projectile its a loss
     public void checkCollision(Player p, int moveDirX, int moveDirY) {
-        // stub
+        Iterator<Wall> iterator1 = wallList.iterator();
+
+        boolean hasHitWall = false;
+
+        while (iterator1.hasNext() && !hasHitWall) {
+            Wall wall = iterator1.next();
+
+            Vector2 wallStartPoint = wall.getStartPoint();  
+            Vector2 wallEndPoint = wall.getEndPoint();
+
+            if ((p.getPosition().isLessThanEqualTo(wallStartPoint) && 
+                p.getPosition().isMoreThanEqualTo(wallEndPoint)) || 
+                (p.getPosition().isLessThanEqualTo(wallEndPoint) && 
+                p.getPosition().isMoreThanEqualTo(wallStartPoint))) {
+                p.move(-moveDirX, -moveDirY);
+                hasHitWall = true;
+            }
+        }
+        
+        Iterator<Projectile> iterator2 = projectileList.iterator();
+
+        boolean hasHitProjectile = false;
+
+        while (iterator2.hasNext() && !hasHitProjectile) {
+            Projectile projectile = iterator2.next();
+
+            Vector2 projectilePos = projectile.getPosition();
+
+            if (p.getPosition().isEqualTo(projectilePos)) {
+                levelLost = true;
+                hasHitProjectile = true;
+            }
+        }
+        // come back to this method
     }
 
     // EFFECTS: returns Level's list of Projectiles
     public LinkedList<Projectile> getProjectiles() {
-        return null; // stub
+        return projectileList;
     }
 
     // EFFECTS: returns Level's list of Walls
     public ArrayList<Wall>  getWalls() {
-        return null; // stub
+        return wallList;
+    }
+
+    // EFFECTS: returns if the Level has been lost
+    public boolean isLevelLost() {
+        return levelLost;
     }
 }
