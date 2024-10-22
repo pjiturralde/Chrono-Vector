d0284b15966b50d4ec93b51c6c44a073e7f2c7e2
DripKnight <90874883+DripKnight@users.noreply.github.com>
2024-10-21 19:04:23 -0700

    Added implementation for JsonReader Class

--- src/main/persistence/JsonReader.java
+++ src/main/persistence/JsonReader.java
@@ -13,37 +13,57 @@ import org.json.*;
 
 // Represents a reader that reads player from JSON data stored in file
 public class JsonReader {
+    private String source;
 
     // EFFECTS: constructs reader to read from source file
     public JsonReader(String source) {
-        // stub
+        this.source = source;
     }
 
     // EFFECTS: reads player from file and returns it;
     // throws IOException if an error occurs reading data from file
     public Player read() throws IOException {
-        return null; // stub
+        String jsonData = readFile(source);
+        JSONObject jsonObject = new JSONObject(jsonData);
+        return parsePlayer(jsonObject);
     }
 
     // EFFECTS: reads source file as string and returns it
     private String readFile(String source) throws IOException {
-        return null; // stub
+        StringBuilder contentBuilder = new StringBuilder();
+
+        try (Stream<String> stream = Files.lines(Paths.get(source), StandardCharsets.UTF_8)) {
+            stream.forEach(s -> contentBuilder.append(s));
+        }
+
+        return contentBuilder.toString();
     }
 
     // EFFECTS: parses player from JSON object and returns it
     private Player parsePlayer(JSONObject jsonObject) {
-        return null; // stub
+        Player player = new Player();
+        addLevelStats(player, jsonObject);
+        return player;
     }
 
     // MODIFIES: player
     // EFFECTS: parses levelstats list from JSON object and adds them to player
     private void addLevelStats(Player player, JSONObject jsonObject) {
-        // stub
+        JSONArray jsonArray = jsonObject.getJSONArray("completedLevelStats");
+        for (Object json : jsonArray) {
+            JSONObject nextLevelStats = (JSONObject) json;
+            addLevelStatsHelper(player, nextLevelStats);
+        }
     }
 
     // MODIFIES: player
     // EFFECTS: parses levelstats from JSON object and adds it to player
     private void addLevelStatsHelper(Player player, JSONObject jsonObject) {
-        // stub
+        String name = jsonObject.getString("name");
+        int leastMovesTaken = jsonObject.getInt("leastMovesTaken");
+        double leastTimeTaken = jsonObject.getDouble("leastTimeTaken");
+        LevelStats stats = new LevelStats(name, leastMovesTaken, leastTimeTaken);
+        player.addCompletedLevelStats(stats);
     }
 }
+
