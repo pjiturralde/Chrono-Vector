2ff227594dfea577c0f159d3cd06f6e4b6d2f699
DripKnight <90874883+DripKnight@users.noreply.github.com>
2024-11-22 10:29:38 -0800

    Added GameplayPanel implementation

--- src/main/ui/GameplayPanel.java
+++ src/main/ui/GameplayPanel.java
@@ -11,6 +11,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+// Gameplay panel
 public class GameplayPanel extends JPanel implements ActionListener{
     private static final int SQUARE_WIDTH = 50;
     private static final int START_DRAW_POINT_X = 1920/3 + 100;
@@ -22,29 +23,52 @@ public class GameplayPanel extends JPanel implements ActionListener{
 
     // EFFECTS: constructs GameplayPanel
     GameplayPanel(Player player) {
-        // stub
+        this.currentLevel = null;
+        this.player = player;
+        this.setBackground(new Color(0,0,44));
+        timer = new Timer(1000/60, this);
+        timer.start();
     }
 
     // MODIFIES: this
     // EFFECTS: sets current level to given level
     public void setCurrentLevel(Level level) {
-        // stub
+        this.currentLevel = level;
     }
 
     // MODIFIES: this
     // EFFECTS: sets this.player to given player
     public void setPlayer(Player player) {
-        // stub
+        this.player = player;
     }
 
     // EFFECTS: paints level and player onto screen
     public void paint(Graphics g) {
-        // stub
+        if (currentLevel != null) {
+            super.paint(g);
+            Graphics2D g2D = (Graphics2D) g;
+    
+            g2D.setPaint(Color.WHITE);
+            for (Wall w : currentLevel.getWalls()) {
+                g2D.fillRect(START_DRAW_POINT_X + w.getStartPoint().getX() * SQUARE_WIDTH, START_DRAW_POINT_Y + w.getStartPoint().getY() * SQUARE_WIDTH, (w.getEndPoint().getX() - w.getStartPoint().getX()) * SQUARE_WIDTH + SQUARE_WIDTH, (w.getEndPoint().getY() - w.getStartPoint().getY()) * SQUARE_WIDTH + SQUARE_WIDTH);
+            }
+    
+            g2D.setPaint(Color.ORANGE);
+            for (Projectile p : currentLevel.getProjectiles()) {
+                g2D.fillRect(START_DRAW_POINT_X + p.getPosition().getX() * SQUARE_WIDTH + 10, START_DRAW_POINT_Y + p.getPosition().getY() * SQUARE_WIDTH + 10, SQUARE_WIDTH - 20, SQUARE_WIDTH - 20);
+            }
+    
+            g2D.setPaint(new Color(0,204,68));
+            g2D.fillRect(START_DRAW_POINT_X + currentLevel.getGoalPosition().getX() * SQUARE_WIDTH, START_DRAW_POINT_Y + currentLevel.getGoalPosition().getY() * SQUARE_WIDTH, SQUARE_WIDTH, SQUARE_WIDTH);
+    
+            g2D.setPaint(Color.WHITE);
+            g2D.fillRect(START_DRAW_POINT_X + player.getPosition().getX() * SQUARE_WIDTH + 10, START_DRAW_POINT_Y + player.getPosition().getY() * SQUARE_WIDTH + 10, SQUARE_WIDTH - 20, SQUARE_WIDTH - 20);
+        }
     }
 
     // EFFECTS: repaints every frame
     @Override
     public void actionPerformed(ActionEvent e) {
-        // stub
+        repaint();
     }
 }
