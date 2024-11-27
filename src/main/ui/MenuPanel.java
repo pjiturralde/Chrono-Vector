c96869628b8174a9d794315f25ea79917414bf0a
DripKnight <90874883+DripKnight@users.noreply.github.com>
2024-11-27 08:53:03 -0800

    Added implementation for MenuPanel class

--- src/main/ui/MenuPanel.java
+++ src/main/ui/MenuPanel.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 import java.awt.*;
 
-// Gameplay panel
+// Menu Panel
 public class MenuPanel extends JPanel{
     private static final int WIDTH = 500;
     private static final int HEIGHT = 600;
@@ -12,11 +12,17 @@ public class MenuPanel extends JPanel{
 
     // EFFECTS: constructs MenuPanel
     MenuPanel() {
-        // stub
+        image = new ImageIcon("src\\resources\\images\\Menu Frame.png").getImage().getScaledInstance(WIDTH, HEIGHT, Image.SCALE_SMOOTH);
+
+        this.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 16));
+        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        this.setBackground(new Color(0, 0, 44));
     }
 
     // EFFECTS: paints level and player onto screen
     public void paintComponent(Graphics g) {
-        // stub
+        super.paintComponent(g);
+
+        g.drawImage(image, 0, 0, this);
     }
 }
