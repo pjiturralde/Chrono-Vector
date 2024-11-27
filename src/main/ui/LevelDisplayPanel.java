69d1850d5b0c8ec749a77ecd52b1129dde500882
DripKnight <90874883+DripKnight@users.noreply.github.com>
2024-11-27 08:45:00 -0800

    Added implementation for LevelDisplayPanel

--- src/main/ui/LevelDisplayPanel.java
+++ src/main/ui/LevelDisplayPanel.java
@@ -4,7 +4,6 @@ import javax.swing.*;
 
 import java.awt.*;
 
-
 // Level Display Panel
 public class LevelDisplayPanel extends JPanel{
     private static final int WIDTH = 380;
@@ -13,11 +12,31 @@ public class LevelDisplayPanel extends JPanel{
 
     // EFFECTS: constructs MenuPanel
     LevelDisplayPanel(String levelName) {
-        // stub
+        image = new ImageIcon("src\\resources\\images\\Level Frame.png").getImage().getScaledInstance(WIDTH, HEIGHT, Image.SCALE_SMOOTH);
+
+        this.setLayout(new BorderLayout());
+        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        this.setBackground(new Color(0, 0, 44));
+
+        JLabel label = new JLabel();
+        label.setPreferredSize(new Dimension(WIDTH, HEIGHT / 3));
+        label.setForeground(new Color(230, 230, 230));
+        label.setFont(new Font("DialogInput", Font.PLAIN, 18));
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setText(levelName);
+
+        JPanel levelDisplayPanel = new JPanel();
+        levelDisplayPanel.setPreferredSize(new Dimension(WIDTH, HEIGHT * (2 / 3)));
+        levelDisplayPanel.setOpaque(false);
+
+        this.add(levelDisplayPanel, BorderLayout.CENTER);
+        this.add(label, BorderLayout.SOUTH);
     }
 
     // EFFECTS: paints level and player onto screen
     public void paintComponent(Graphics g) {
-        // stub
+        super.paintComponent(g);
+
+        g.drawImage(image, 0, 0, this);
     }
 }
