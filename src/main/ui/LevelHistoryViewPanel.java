eda713ed1ebfe9e879fa5551b9574b8ab3d83200
DripKnight <90874883+DripKnight@users.noreply.github.com>
2024-11-27 08:55:58 -0800

    Added specification for LevelHistoryViewPanel class

---
+++ src/main/ui/LevelHistoryViewPanel.java
@@ -0,0 +1,37 @@
+package ui;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+import model.Level;
+import model.Player;
+import model.Projectile;
+import model.Vector2;
+import model.Wall;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+
+// Level History View Panel
+public class LevelHistoryViewPanel extends JPanel implements ActionListener {
+
+    // EFFECTS: constructs LevelHistoryViewPanel
+    LevelHistoryViewPanel() {
+        // stub
+    }
+
+    // EFFECTS: paints level and player onto screen
+    public void paint(Graphics g) {
+        // stub
+    }
+
+    // EFFECTS: repaints every frame and handles particle position
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // stub
+    }
+}
