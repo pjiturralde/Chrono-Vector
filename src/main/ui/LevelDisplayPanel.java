a5d34d979aec6acdfc37c0ac7a9c57dfff7347e0
DripKnight <90874883+DripKnight@users.noreply.github.com>
2024-11-27 08:44:19 -0800

    Added specification for LevelDisplayPanel

---
+++ src/main/ui/LevelDisplayPanel.java
@@ -0,0 +1,23 @@
+package ui;
+
+import javax.swing.*;
+
+import java.awt.*;
+
+
+// Level Display Panel
+public class LevelDisplayPanel extends JPanel{
+    private static final int WIDTH = 380;
+    private static final int HEIGHT = 500;
+    private Image image;
+
+    // EFFECTS: constructs MenuPanel
+    LevelDisplayPanel(String levelName) {
+        // stub
+    }
+
+    // EFFECTS: paints level and player onto screen
+    public void paintComponent(Graphics g) {
+        // stub
+    }
+}
