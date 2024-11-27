fa948f1854900a4b0b1fbbdc4a55a14f36f6c81a
DripKnight <90874883+DripKnight@users.noreply.github.com>
2024-11-27 08:50:57 -0800

    Added specification for MenuPanel class

---
+++ src/main/ui/MenuPanel.java
@@ -0,0 +1,22 @@
+package ui;
+
+import javax.swing.*;
+
+import java.awt.*;
+
+// Gameplay panel
+public class MenuPanel extends JPanel{
+    private static final int WIDTH = 500;
+    private static final int HEIGHT = 600;
+    private Image image;
+
+    // EFFECTS: constructs MenuPanel
+    MenuPanel() {
+        // stub
+    }
+
+    // EFFECTS: paints level and player onto screen
+    public void paintComponent(Graphics g) {
+        // stub
+    }
+}
