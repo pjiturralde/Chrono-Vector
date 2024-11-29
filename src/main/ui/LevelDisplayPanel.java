ea33871ed6f6dbdb1304f7919f748b44e11500c3
DripKnight <90874883+DripKnight@users.noreply.github.com>
2024-11-29 06:38:31 -0800

    Updated implementation for LevelDisplayPanel class

--- src/main/ui/LevelDisplayPanel.java
+++ src/main/ui/LevelDisplayPanel.java
@@ -5,27 +5,33 @@ import javax.swing.*;
 import java.awt.*;
 
 // Level Display Panel
-public class LevelDisplayPanel extends JPanel{
-    private static final int WIDTH = 380;
-    private static final int HEIGHT = 500;
+public class LevelDisplayPanel extends JPanel {
+    private JLabel label;
+    private JPanel levelDisplayPanel;
+    private int width;
+    private int height;
     private Image image;
 
-    // EFFECTS: constructs MenuPanel
-    LevelDisplayPanel(String levelName) {
-        image = new ImageIcon("src\\resources\\images\\Level Frame.png").getImage().getScaledInstance(WIDTH, HEIGHT, Image.SCALE_SMOOTH);
+    // EFFECTS: constructs LevelDisplayPanel
+    LevelDisplayPanel(String levelName, int width, int height) {
+        image = new ImageIcon("src/resources/images/Level Frame.png").getImage().getScaledInstance(width, height,
+                Image.SCALE_SMOOTH);
+
+        this.width = width;
+        this.height = height;
 
         this.setLayout(new BorderLayout());
         this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
         this.setBackground(new Color(0, 0, 44));
 
-        JLabel label = new JLabel();
+        label = new JLabel();
         label.setPreferredSize(new Dimension(WIDTH, HEIGHT / 3));
         label.setForeground(new Color(230, 230, 230));
         label.setFont(new Font("DialogInput", Font.PLAIN, 18));
         label.setHorizontalAlignment(JLabel.CENTER);
         label.setText(levelName);
 
-        JPanel levelDisplayPanel = new JPanel();
+        levelDisplayPanel = new JPanel();
         levelDisplayPanel.setPreferredSize(new Dimension(WIDTH, HEIGHT * (2 / 3)));
         levelDisplayPanel.setOpaque(false);
 
@@ -33,10 +39,20 @@ public class LevelDisplayPanel extends JPanel{
         this.add(label, BorderLayout.SOUTH);
     }
 
+    // MODIFIES: this
+    // EFFECTS: changes the dimensions of this
+    public void setDimension(int width, int height) {
+        this.width = width;
+        this.height = height;
+
+        levelDisplayPanel.setPreferredSize(new Dimension(width, height * (2 / 3)));
+        label.setPreferredSize(new Dimension(width, height / 3));
+    }
+
     // EFFECTS: paints level and player onto screen
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.drawImage(image, 0, 0, this);
+        g.drawImage(image, 0, 0, width, height, this);
     }
 }
