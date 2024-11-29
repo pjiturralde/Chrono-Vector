fd4f4ca99478ff5cf017572cde401a898dfb503a
DripKnight <90874883+DripKnight@users.noreply.github.com>
2024-11-29 06:39:36 -0800

    Updated implementation for MenuPanel class

--- src/main/ui/MenuPanel.java
+++ src/main/ui/MenuPanel.java
@@ -5,24 +5,39 @@ import javax.swing.*;
 import java.awt.*;
 
 // Menu Panel
-public class MenuPanel extends JPanel{
-    private static final int WIDTH = 500;
-    private static final int HEIGHT = 600;
+public class MenuPanel extends JPanel {
+    private int width = 500;
+    private int height = 600;
     private Image image;
 
     // EFFECTS: constructs MenuPanel
     MenuPanel() {
-        image = new ImageIcon("src\\resources\\images\\Menu Frame.png").getImage().getScaledInstance(WIDTH, HEIGHT, Image.SCALE_SMOOTH);
+        image = new ImageIcon("src/resources/images/Menu Frame.png").getImage().getScaledInstance(width, height,
+                Image.SCALE_SMOOTH);
 
         this.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 16));
-        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        this.setPreferredSize(new Dimension(width, height));
         this.setBackground(new Color(0, 0, 44));
+    }
+
+    // MODIFIES: this
+    // EFFECTS: resizes panel to given width and height
+    public void resizePanel(int width, int height) {
+        final double FRAME_TO_VGAP_RATIO = 0.025;
+
+        this.width = width;
+        this.height = height;
+
+        this.setPreferredSize(new Dimension(width, height));
+        this.setLayout(new FlowLayout(FlowLayout.CENTER, 10, (int) Math.round(height * FRAME_TO_VGAP_RATIO)));
+        revalidate();
+        repaint();
     }
 
     // EFFECTS: paints level and player onto screen
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.drawImage(image, 0, 0, this);
+        g.drawImage(image, 0, 0, width, height, this);
     }
 }
