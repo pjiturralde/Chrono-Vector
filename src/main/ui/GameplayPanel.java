e3b37a0b4bd53142eb87de94bda7ec723429a9ac
DripKnight <90874883+DripKnight@users.noreply.github.com>
2024-11-29 06:36:09 -0800

    Updated implementation for GameplayPanel class

--- src/main/ui/GameplayPanel.java
+++ src/main/ui/GameplayPanel.java
@@ -2,8 +2,6 @@ package ui;
 
 import javax.swing.*;
 
-import model.Level;
-import model.Player;
 import model.Projectile;
 import model.Wall;
 
@@ -13,65 +11,85 @@ import java.awt.event.ActionListener;
 
 // Gameplay panel
 public class GameplayPanel extends JPanel implements ActionListener {
-    private static final int SQUARE_WIDTH = 50;
-    private static final int START_DRAW_POINT_X = 1920 / 3 + 100;
-    private static final int START_DRAW_POINT_Y = 280;
+    private int squareWidth = 50;
+    private int startPointX = 1920 / 3;
+    private int startPointY = 280;
 
-    Timer timer;
-    Level currentLevel;
-    Player player;
+    private GameApp gameApp;
+    private Timer timer;
 
     // EFFECTS: constructs GameplayPanel
-    GameplayPanel(Player player) {
-        this.currentLevel = null;
-        this.player = player;
+    GameplayPanel(GameApp gameApp) {
+        this.gameApp = gameApp;
         this.setBackground(new Color(0, 0, 44));
         timer = new Timer(1000 / 60, this);
         timer.start();
     }
 
     // MODIFIES: this
-    // EFFECTS: sets current level to given level
-    public void setCurrentLevel(Level level) {
-        this.currentLevel = level;
-    }
+    // EFFECTS: resizes panel and components to given width and height
+    public void resizePanel(int width, int height) {
+        final float SCREEN_TO_SQUARE_WIDTH_RATIO = 0.05f;
+        final float SCREEN_TO_START_DRAW_POINT_X_RATIO = 0.5f;
+        final float SCREEN_TO_START_DRAW_POINT_Y_RATIO = 0.2f;
 
-    // MODIFIES: this
-    // EFFECTS: sets this.player to given player
-    public void setPlayer(Player player) {
-        this.player = player;
+        squareWidth = Math.round(height * SCREEN_TO_SQUARE_WIDTH_RATIO);
+        startPointX = Math.round(width * SCREEN_TO_START_DRAW_POINT_X_RATIO
+                - (gameApp.getCurrentLevel().getSize().getX() + 1) * squareWidth / 2);
+        startPointY = Math.round(height * SCREEN_TO_START_DRAW_POINT_Y_RATIO);
+
+        this.setBounds(0, 0, width, height);
+        revalidate();
+        repaint();
     }
 
     // EFFECTS: paints level and player onto screen
     public void paint(Graphics g) {
-        if (currentLevel != null) {
+        if (gameApp.getCurrentLevel() != null) {
             super.paint(g);
             Graphics2D g2D = (Graphics2D) g;
 
-            g2D.setPaint(Color.WHITE);
-            for (Wall w : currentLevel.getWalls()) {
-                g2D.fillRect(START_DRAW_POINT_X + w.getStartPoint().getX() * SQUARE_WIDTH,
-                        START_DRAW_POINT_Y + w.getStartPoint().getY() * SQUARE_WIDTH,
-                        (w.getEndPoint().getX() - w.getStartPoint().getX()) * SQUARE_WIDTH + SQUARE_WIDTH,
-                        (w.getEndPoint().getY() - w.getStartPoint().getY()) * SQUARE_WIDTH + SQUARE_WIDTH);
-            }
+            paintWalls(g2D);
+            paintProjectiles(g2D);
+            paintGoal(g2D);
+            paintPlayer(g2D);
+        }
+    }
 
-            g2D.setPaint(Color.ORANGE);
-            for (Projectile p : currentLevel.getProjectiles()) {
-                g2D.fillRect(START_DRAW_POINT_X + p.getPosition().getX() * SQUARE_WIDTH + 10,
-                        START_DRAW_POINT_Y + p.getPosition().getY() * SQUARE_WIDTH + 10, SQUARE_WIDTH - 20,
-                        SQUARE_WIDTH - 20);
-            }
+    // EFFECTS: paints player onto this panel
+    public void paintPlayer(Graphics2D g2D) {
+        g2D.setPaint(Color.WHITE);
+        g2D.fillRect(startPointX + gameApp.getPlayer().getPosition().getX() * squareWidth + 10,
+                startPointY + gameApp.getPlayer().getPosition().getY() * squareWidth + 10, squareWidth - 20,
+                squareWidth - 20);
+    }
 
-            g2D.setPaint(new Color(0, 204, 68));
-            g2D.fillRect(START_DRAW_POINT_X + currentLevel.getGoalPosition().getX() * SQUARE_WIDTH,
-                    START_DRAW_POINT_Y + currentLevel.getGoalPosition().getY() * SQUARE_WIDTH, SQUARE_WIDTH,
-                    SQUARE_WIDTH);
+    // EFFECTS: paints goal onto this panel
+    public void paintGoal(Graphics2D g2D) {
+        g2D.setPaint(new Color(0, 204, 68));
+        g2D.fillRect(startPointX + gameApp.getCurrentLevel().getGoalPosition().getX() * squareWidth,
+                startPointY + gameApp.getCurrentLevel().getGoalPosition().getY() * squareWidth, squareWidth,
+                squareWidth);
+    }
 
-            g2D.setPaint(Color.WHITE);
-            g2D.fillRect(START_DRAW_POINT_X + player.getPosition().getX() * SQUARE_WIDTH + 10,
-                    START_DRAW_POINT_Y + player.getPosition().getY() * SQUARE_WIDTH + 10, SQUARE_WIDTH - 20,
-                    SQUARE_WIDTH - 20);
+    // EFFECTS: paints projectiles onto this panel
+    public void paintProjectiles(Graphics2D g2D) {
+        g2D.setPaint(Color.ORANGE);
+        for (Projectile p : gameApp.getCurrentLevel().getProjectiles()) {
+            g2D.fillRect(startPointX + p.getPosition().getX() * squareWidth + 10,
+                    startPointY + p.getPosition().getY() * squareWidth + 10, squareWidth - 20,
+                    squareWidth - 20);
+        }
+    }
+
+    // EFFECTS: paints walls onto this panel
+    public void paintWalls(Graphics2D g2D) {
+        g2D.setPaint(Color.WHITE);
+        for (Wall w : gameApp.getCurrentLevel().getWalls()) {
+            g2D.fillRect(startPointX + w.getStartPoint().getX() * squareWidth,
+                    startPointY + w.getStartPoint().getY() * squareWidth,
+                    (w.getEndPoint().getX() - w.getStartPoint().getX()) * squareWidth + squareWidth,
+                    (w.getEndPoint().getY() - w.getStartPoint().getY()) * squareWidth + squareWidth);
         }
     }
 
